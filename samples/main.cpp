726b6a34af6915327c39621660866bccb221eb6e
Andrew F <flamewave000@live.com>
2017-08-28 10:55:17 -0400

    Expanded example code to show class level format

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
+#include <limits>
 #include "../include/stringx.h"
 
 using namespace std;
 using namespace strx;
 
+struct name_pair { string first, last; };
+format& operator%(format &&fmt, const name_pair &pair) {
+	fmt.append(pair.first + ' ' + pair.last);
+	return fmt;
+}
 
 int main() {
-
-	string firstName, lastName;
+	name_pair name;
 	int age = 0;
 	float weight = 0;
 	cout << "Please enter your first name: " << flush;
-	cin >> firstName;
+	cin >> name.first;
 	cout << "Please enter your last name: " << flush;
-	cin >> lastName;
+	cin >> name.last;
 	cout << "Please enter your age: " << flush;
 	cin >> age;
 	cout << "Please enter your weight: " << flush;
 	cin >> weight;
 
 	cout <<
-		"\nHello {0} {1}\n{2}: {3}\n{4}: {5}\n"_f
-		% firstName
-		% lastName
+		"\nHello {0}\n{1}: {2}\n{3}: {4}\n"_f
+		% name
 		% lpad("age", 6)
 		% age
 		% "weight"
 		% spec("%.0f", weight)
 		<< endl;
 
-	string dev_null;
-	cout << "Enter anything to exit: " << flush;
-	cin >> dev_null;
+	cout << "Press any key to continue..." << flush;
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	cin.get();
 
 	return 0;
 }
